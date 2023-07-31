a1a6fa5d60bb94d0a089ee8e511c4db8cf5ab4a8
ShotaTanemura <shota0711tane@gmail.com>
2023-07-31 16:50:51 +0900

    Add setup_gtest.sh

---
+++ setup_gtest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+
+if !(type "brew" > /dev/null 2>&1); then
+	curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | bash
+	brew update && brew upgrade && brew install readline
+fi
+
+if !(type "Ninja" > /dev/null 2>&1); then
+	brew install Ninja
+fi
+
+if !(type "Cmake" > /dev/null 2>&1); then
+	brew install Cmake
+fi
+
+mkdir tests
+git clone https://github.com/google/googletest.git googletest
+cd googletest 
+
+cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX=../tests
+cmake --build build
+cmake --install build
